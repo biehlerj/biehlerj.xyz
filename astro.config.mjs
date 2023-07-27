d7971aaf8d9ca4839fb12e5c33ab5ffcd3acac20
Jacob Biehler <jacob@biehlerj.xyz>
2023-07-26 18:41:37 -0700

    feat(tailwind): Adding tailwind and theme toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        assets: true,
-    },
-    site: "https://biehlerj.github.io",
+  experimental: {
+    assets: true
+  },
+  site: "https://biehlerj.github.io",
+  integrations: [tailwind()]
 });
